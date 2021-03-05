82e435b37a60c8bd08f9f76f518a7a71405077bf
Alexandre Brisebois <brisebois@outlook.com>
2021-03-05 11:37:48 -0500

    added app insights

--- src/megaphone.resources/Startup.cs
+++ src/megaphone.resources/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
 
 namespace Megaphone.Resources
 {
@@ -30,6 +31,10 @@ namespace Megaphone.Resources
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Megaphone.Resources", Version = "v1" });
             });
+
+            string key = Environment.GetEnvironmentVariable("INSTRUMENTATION_KEY");
+            if (!string.IsNullOrEmpty(key))
+                services.AddApplicationInsightsTelemetry(key);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
